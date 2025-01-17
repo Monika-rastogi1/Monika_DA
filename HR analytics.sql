3a2c7c734a4046094034e59d2344be41be33c321
Monika-rastogi1 <136727763+Monika-rastogi1@users.noreply.github.com>
2025-01-17 13:24:45 +0530

    HR analytics.sql

--- HR analytics.sql
+++ HR analytics.sql
@@ -1,10 +1,14 @@
+---- Create Database for Employee attrition analysis 
+
 create database HR;
 use HR;
+
+---- Importing data from excel files by Table data import wizard
 select*from hr_1;
 select*from hr_2;
 drop table hr_2;
 
-###KPI 1
+--- KPI 1 Average Attrition Rate for All Departments
 select a.Department, concat(format(avg(a.attrition_y)*100,2),'%') as Attrition_Rate
 from  
 ( select department,attrition,
@@ -14,11 +18,11 @@ Else 0
 End as attrition_y from hr_1 ) as a
 group by department;
 
-####KPI 2
+---- KPI 2 Average Hourly Rate of Male Research Scientists
 select avg(HourlyRate)as average_hourly_rate, JobRole, Gender from hr_1
 where JobRole='Research Scientist' and Gender='Male';
 
-###KPI 3
+---- KPI 3 Attrition Rate vs Monthly Income Stats
 select a.department, concat(format(avg(a.attrition_rate)*100,2),'%') as Average_attrition,format(avg(b.monthlyincome),2) as Average_Monthly_Income
 from ( select department,attrition,EmployeeNumber,
 case when attrition = 'yes' then 1
@@ -28,13 +32,13 @@ inner join hr_2 as b on b.EmployeeID = a.EmployeeNumber
 group by a.department;
 
 
-####KPI 4
+---- KPI 4 Average Working Years for Each Department
 select a.department, avg(b.TotalWorkingYears) as average_working_years
 from hr_1 as a 
 inner join hr_2 as b on b.EmployeeID=a.EmployeeNumber
 group by a.department;
 
-###KPI 5
+---- KPI 5 Job Role vs Work-Life Balance
 select a.JobRole,
 sum(case when performancerating = 1 then 1 else 0 end) as 1st_Rating_Total,
 sum(case when performancerating = 2 then 1 else 0 end) as 2nd_Rating_Total,
@@ -45,7 +49,7 @@ from hr_1 as a
 inner join hr_2 as b on b.EmployeeID = a.EmployeeNumber
 group by a.jobrole;
 
-###KPI 6
+---- KPI 6 Attrition Rate vs Years Since Last Promotion Relation
 select a.JobRole,concat(format(avg(a.attrition_rate)*100,2),'%') as Average_Attrition_Rate,
 format(avg(b.YearsSinceLastPromotion),2) as Average_YearsSinceLastPromotion
 from ( select JobRole,attrition,EmployeeNumber,
